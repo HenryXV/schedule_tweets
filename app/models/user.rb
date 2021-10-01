19703cfeac7ddf144891be80e13242bf5ffc0656
Henrique Resende Silva <henri.r.s@hotmail.com>
2021-10-01 00:06:37 -0300

    Twitter API and Schedule tweet implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@
 # password:string virtual
 # password_configuration:string virtual
 class User < ApplicationRecord
+  has_many :twitter_accounts
+  has_many :tweets
+
   has_secure_password
 
   validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
